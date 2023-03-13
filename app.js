f61eec16b2bc9e6a0fda03cceafc6190cb33ee05
jaimeespoz <jespoz@outlook.es>
2023-03-12 21:47:29 -0300

    e

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 var express = require('express'),
 	/* routes = require("./routes"), */
-	publicDir = express.static(`${__dirname}/public`),
-	viewDir = '${__dirname}/views',
+	publicDir = `${__dirname}/public`,
+	viewDir = `${__dirname}/views`,
 	port = process.env.PORT || 3000,
 	app = express();
 
